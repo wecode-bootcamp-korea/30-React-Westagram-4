59bd7d25d0058c9bc0062dbe75493b65f1e3c4c0
YunJi Shin <yunji@YunJiui-MacBookPro.local>
2022-02-20 20:25:33 +0900

    Add: Mission 1 - 사용자 입력 데이터 저장 기능 구현

--- src/pages/yunjiShin/Login/ShinLogin.js
+++ src/pages/yunjiShin/Login/ShinLogin.js
@@ -4,6 +4,51 @@ import { useNavigate } from 'react-router-dom';
 import './ShinLogin.scss';
 
 const ShinLogin = () => {
-  console.log(123);
+  const [id, setId] = useState('');
+  const [pw, setPw] = useState('');
+  const navigate = useNavigate();
+
+  const handleIdInput = e => {
+    setId(e.target.value);
+  };
+  const handlePwInput = e => {
+    setPw(e.target.value);
+  };
+
+  const goToMain = () => {
+    navigate('/main');
+  };
+
+  return (
+    <div className="login">
+      <h1>Westagram</h1>
+      <section>
+        <div>
+          <input
+            type="text"
+            placeholder="  전화번호, 사용자 이름 또는 이메일"
+            id="inputId"
+            onChange={handleIdInput}
+          />
+        </div>
+        <div>
+          <input
+            type="password"
+            placeholder="  비밀번호"
+            id="inputPw"
+            onChange={handlePwInput}
+          />
+        </div>
+        <button type="button" id="btnLogin" onClick={goToMain}>
+          로그인
+        </button>
+      </section>
+      <div id="forgetPw">
+        <a href="https://www.instagram.com/accounts/password/reset/">
+          비밀번호를 잊으셨나요?
+        </a>
+      </div>
+    </div>
+  );
 };
 export default ShinLogin;
--- src/pages/yunjiShin/Main/ShinMain.js
+++ src/pages/yunjiShin/Main/ShinMain.js
@@ -3,7 +3,150 @@ import React from 'react';
 import './ShinMain.scss';
 
 const ShinMain = () => {
-  console.log(123);
+  return (
+    <div className="main">
+      <nav>
+        <section className="navTopLogo">
+          <img
+            alt="instagram logo"
+            className="instagramLogo"
+            src="/images/instagram.png"
+          />
+          <span className="instagram">Instagram</span>
+        </section>
+        <span className="searchBar">
+          <img
+            alt="search icon"
+            className="searchIcon"
+            src="/images/search.png"
+          />
+          검색
+        </span>
+        <section className="navRight">
+          <img
+            alt="explore icon"
+            className="exploreIcon"
+            src="/images/search.png"
+          />
+          <img
+            alt="heart icon"
+            className="heartIcon"
+            src="/images/instagram_heart.png"
+          />
+          <img
+            alt="profile icon"
+            className="profileIcon"
+            src="/images/instagram_profile.png"
+          />
+        </section>
+      </nav>
+      <main>
+        <div id="feeds">
+          <article>
+            <section className="feedArticle">
+              <div className="feedArticleProfile">
+                <img
+                  alt="feed profile"
+                  className="feedProfile"
+                  src="/images/good-faces-ouWf8vF1NSo-unsplash.jpg"
+                />
+                <span className="feedNickname">human_oo1 </span>
+              </div>
+            </section>
+            <section>
+              <img
+                alt="feed article"
+                className="feedPicture"
+                src="/images/photo-1644780295307-eab5f4f430a3.jpeg"
+              />
+            </section>
+            <section className="feedBtn">
+              <div>
+                <img
+                  alt="heart button"
+                  className="heartBtn"
+                  src="/images/instagram_heart.png"
+                />
+                <img
+                  alt="comment button"
+                  className="commentBtn"
+                  src="/images/chat-bubble.png"
+                />
+                <img
+                  alt="export button"
+                  className="shareBtn"
+                  src="/images/premium-icon-export-3024597.png"
+                />
+              </div>
+            </section>
+          </article>
+          <section className="likes">
+            <img
+              alt="right profile1"
+              className="mainRightStoryProfile1"
+              src="/images/collins-lesulie-h8F09Am8W5Y-unsplash.jpg"
+            />
+            <span>yourfriendoo1님 외 7명이 좋아합니다.</span>
+          </section>
+          <section className="description">
+            <span className="descriptionNickname"> human_oo1 </span>
+            <span> 바다 #today </span>
+          </section>
+          <section className="comments" />
+        </div>
+        <div id="main-right">
+          <div className="mainRightMe">
+            <img
+              alt="right profile0"
+              className="myProfile"
+              src="/images/alexa-portoraro-7UuwLkA8rHQ-unsplash.jpg"
+            />
+            <p className="myId">mememememe</p>
+            <p className="myNickname">myself | coding</p>
+          </div>
+          <section className="mainRightStory">
+            <div className="mainRightStoryIndex">
+              <h3 className="stories">스토리</h3>
+              <h3 className="seeAll">모두 보기</h3>
+            </div>
+            <div className="mainRightStory1">
+              <img
+                alt="story1 profile"
+                className="mainRightStoryProfile1"
+                src="/images/collins-lesulie-h8F09Am8W5Y-unsplash.jpg"
+              />
+              <span>yourfriendoo1</span>
+              <span className="mainRightStoryTime1">19분 전</span>
+            </div>
+            <div className="mainRightStory2">
+              <img
+                alt="story2 profile"
+                className="mainRightStoryProfile2"
+                src="/images/hamza-nouasria-dPEcZi445OM-unsplash.jpg"
+              />
+              <span>yourfriendoo2</span>
+              <span className="mainRightStoryTime2">49분 전</span>
+            </div>
+            <div className="mainRightStory3">
+              <img
+                alt="right profile3"
+                className="mainRightStoryProfile3"
+                src="/images/photo-1553272725-086100aecf5e.jpeg"
+              />
+              <span>yourfriendoo33</span>
+              <span className="mainRightStoryTime3">4시간 전</span>
+            </div>
+          </section>
+          <section className="mainRightSuggestions">
+            <div className="mainRightSuggestionsIndex">
+              <h3 className="suggestions">회원님을 위한 추천</h3>
+              <h3 className="seeAll">모두 보기</h3>
+            </div>
+          </section>
+        </div>
+      </main>
+    </div>
+  );
 };
 
 export default ShinMain;
