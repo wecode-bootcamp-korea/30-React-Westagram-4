eaafd559eee418e9388c465778f41fce8f70004e
안광민 <kwangmon@angwangmin-ui-MacBookPro.local>
2022-02-21 20:32:57 +0900

    Add: Login 기능 구현

--- src/pages/kwangminAhn/Login/AhnLogin.js
+++ src/pages/kwangminAhn/Login/AhnLogin.js
@@ -1,9 +1,75 @@
 import React from 'react';
 import { useNavigate } from 'react-router-dom';
+import { useState } from 'react';
 
 import './AhnLogin.scss';
 
 const AhnLogin = () => {
-  console.log(123);
+  const navigate = useNavigate();
+
+  const goToMain = () => {
+    navigate('/AhnMain');
+  };
+
+  const [userId, setUserId] = useState('');
+
+  const handleIdInput = e => {
+    setUserId(e.target.value);
+  };
+
+  const [userPassword, SetUserPassword] = useState('');
+
+  const handlePasswordInput = e => {
+    SetUserPassword(e.target.value);
+  };
+
+  return (
+    <div className="login">
+      <h1>Westagram</h1>
+      <div className="id">
+        <input
+          id="loginId"
+          type="text"
+          placeholder="전화번호, 사용자 이름 또는 이메일"
+          value={userId}
+          onChange={handleIdInput}
+        />
+      </div>
+      <div className="password">
+        <input
+          id="loginPassword"
+          type="password"
+          placeholder="비밀번호"
+          value={userPassword}
+          onChange={handlePasswordInput}
+        />
+      </div>
+      <div className="btn">
+        {/* <button
+          className="loginButton"
+          disabled={!(userId.indexOf('@') > -1 && userPassword.length >= 5)}
+          onClick={goToMain}
+        >
+          로그인
+        </button> */}
+        <button
+          className={`loginButton ${
+            userId.indexOf('@') > -1 && userPassword.length >= 5
+              ? 'cssloginButton'
+              : ''
+          }`}
+          onClick={goToMain}
+        >
+          로그인
+        </button>
+      </div>
+      <div className="link">
+        <a href="#" className="footerLink">
+          비밀번호를 잊으셨나요?
+        </a>
+      </div>
+    </div>
+  );
 };
+
 export default AhnLogin;
